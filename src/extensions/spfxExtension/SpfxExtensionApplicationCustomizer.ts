bcabb170958aa953e767e799987771c4dd20a852
ProjectPointInc <jd@ProjectPointInc.com>
2019-06-21 22:06:26 -0700

    message

--- src/extensions/spfxExtension/SpfxExtensionApplicationCustomizer.ts
+++ src/extensions/spfxExtension/SpfxExtensionApplicationCustomizer.ts
@@ -4,18 +4,17 @@ import { BaseApplicationCustomizer, PlaceholderName, PlaceholderContent } from '
 //import { Dialog } from '@microsoft/sp-dialog';
 //import * as strings from 'SpfxExtensionApplicationCustomizerStrings';
 
-//declare var $: any;
+declare var $: any;
+require('jquery');
+require('modal');
 
-//require('jquery');
-//require('modal');
+import { SPComponentLoader } from '@microsoft/sp-loader';
 
-//import { SPComponentLoader } from '@microsoft/sp-loader';
-/*
 import {
   SPHttpClient,
   SPHttpClientResponse,
-//  SPHttpClientConfiguration,
-//  ISPHttpClientOptions
+  //  SPHttpClientConfiguration,
+  //  ISPHttpClientOptions
 } from '@microsoft/sp-http';
 
 
@@ -27,24 +26,25 @@ export interface ISpfxExtensionApplicationCustomizerProperties {
   url: string;
 }
 
-interface IListItem {
-  // Title?: string;
+export interface IListItem {
+  Title?: string;
   // EmailAddress: string;
   Id: number;
-  CompletedEnrollment: boolean;
+  //CompletedEnrollment: boolean;
+  EnrollmentCompleted: Date;
 }
-
 const redirectUrl: string = 'https://6sc.sharepoint.com/sites/TPBC/SitePages/ThankYou.aspx';
-*/
-
-export interface ISpfxExtensionApplicationCustomizerProperties {}
 const LOG_SOURCE: string = 'SpfxExtensionApplicationCustomizer';
-
 export default class SpfxExtensionApplicationCustomizer extends BaseApplicationCustomizer<ISpfxExtensionApplicationCustomizerProperties> {
-/*
+  private listItemEntityTypeName: string = undefined;
+  private _topPlaceholder: PlaceholderContent | undefined;
+  constructor() {
+    super();
+    SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css');
+  }
   private getLatestItemId(): Promise<number> {
     return new Promise<number>((resolve: (itemId: number) => void, reject: (error: any) => void): void => {
-      this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Enrollments')/items?$orderby=Id desc&$top=1&$select=id&$filter=UserEmail+eq+'${this.properties.userEmail}'+and+CompletedEnrollment+eq+0`,
+      this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('Enrollments')/items?$orderby=Id desc&$top=1&$select=id&$filter=UserEmail+eq+'${this.properties.userEmail}'+and+EnrollmentCompleted+eq+null`,
         SPHttpClient.configurations.v1,
         {
           headers: {
@@ -65,7 +65,7 @@ export default class SpfxExtensionApplicationCustomizer extends BaseApplicationC
         });
     });
   }
-*/
+
   private InsertJSFile(url: string): void {
     let head: any = document.getElementsByTagName("head")[0] || document.documentElement;
     let script: HTMLScriptElement = document.createElement("script");
@@ -76,49 +76,167 @@ export default class SpfxExtensionApplicationCustomizer extends BaseApplicationC
   }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
   @override
   public onInit(): Promise<void> {
-    /*
+
     Log.info(LOG_SOURCE, "Initialized ");
     this.properties.url = this.context.pageContext.web.absoluteUrl;
     this.properties.userEmail = this.context.pageContext.user.email.toString();
     this.properties.listName = "Enrollments";
-    */
 
-    this.InsertJSFile(`${this.context.pageContext.web.absoluteUrl}/SiteAssets/js/jquery-3.4.1.min.js`);
+    //this.InsertJSFile(`${this.context.pageContext.web.absoluteUrl}/SiteAssets/js/jquery-3.4.1.min.js`);
     //this.InsertJSFile(`https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js`);
     //this.InsertJSFile(`https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js`);
     //this.InsertJSFile(`${this.properties.url}/SiteAssets/js/site.js`);
     this.InsertJSFile(`${this.context.pageContext.web.absoluteUrl}/SiteAssets/js/site.js`);
-    return Promise.resolve();
-  }
-}
-  /*
 
     //  only run for external user
     if (this.context.pageContext.user.isExternalGuestUser || this.context.pageContext.user.isAnonymousGuestUser) {
       // do not run on Thank you page or enrollment form
-      //if ((document.location.href).toLowerCase().indexOf("thankyou.aspx") == -1 && (document.location.href).toLowerCase().indexOf("enrollment.aspx") == -1) {
-        //let restCall: string = `${this.properties.url}/_api/web/lists/getbytitle('Enrollments')/items?&$filter=UserEmail+eq+'${this.properties.userEmail}'+and+CompletedEnrollment+eq+1`;
-        //this.ItemExists(restCall).then((result) => {
-        this.getLatestItemId().then((result) => {
-          this.properties.itemId = result;
-          if (this.properties.itemId > 0) {    //  only update probably should add new item   ///   js/site.js
-            //    this.InsertJSFile("https://6sc.sharepoint.com/sites/TPBC/SiteAssets/js/site.js");  // includes js in site assets library
-            //this.showIframe(); // works for classic form or modern form or powerapp form   `https://web.powerapps.com/webplayer/iframeapp?hidenavbar=true&amp;screenColor=white&amp;appId=/providers/Microsoft.PowerApps/apps/473cc4ab-6455-463b-8f23-08a0ab89b856&amp;userEmail=${this.properties.userEmail}"`
-           //this.showForm();   /// displays form in modal
-            this.InsertJSFile(`${this.properties.url}/SiteAssets/js/notEnrolled.js`);
-          }
-        })
-          .catch((error: any) => {
-            console.log(error);
-            return true;  ///  log the error and return true so user can continue
-          });
+      if ((document.location.href).toLowerCase().indexOf("thankyou.aspx") == -1 && (document.location.href).toLowerCase().indexOf("enrollment.aspx") == -1) {
+      //let restCall: string = `${this.properties.url}/_api/web/lists/getbytitle('Enrollments')/items?&$filter=UserEmail+eq+'${this.properties.userEmail}'+and+CompletedEnrollment+eq+1`;
+      //this.ItemExists(restCall).then((result) => {
+      this.getLatestItemId().then((result) => {
+        this.properties.itemId = result;
+        if (this.properties.itemId > 0) {    //  only update probably should add new item   ///   js/site.js
+          //    this.InsertJSFile("https://6sc.sharepoint.com/sites/TPBC/SiteAssets/js/site.js");  // includes js in site assets library
+          //this.showIframe(); // works for classic form or modern form or powerapp form   `https://web.powerapps.com/webplayer/iframeapp?hidenavbar=true&amp;screenColor=white&amp;appId=/providers/Microsoft.PowerApps/apps/473cc4ab-6455-463b-8f23-08a0ab89b856&amp;userEmail=${this.properties.userEmail}"`
+          this.showForm();   /// displays form in modal
+          this.InsertJSFile(`${this.properties.url}/SiteAssets/js/notEnrolled.js`);
+        }
+      })
+        .catch((error: any) => {
+          console.log(error);
+          return true;  ///  log the error and return true so user can continue
+        });
     }
+  }
     return Promise.resolve();
   }
-  /*
- /*
+
+  public FormSave(): void {  
+    let latestItemId: number = undefined;  
+    this.updateStatus('Loading latest item...');  
+    
+    this.getLatestItemId()  
+      .then((itemId: number): Promise<SPHttpClientResponse> => {  
+        if (itemId === -1) {  
+          throw new Error('No items found in the list');  
+        }  
+    
+        latestItemId = itemId;  
+        this.updateStatus(`Loading information about item ID: ${itemId}...`);  
+          
+        return this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${latestItemId})?$select=Title,Id`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'odata-version': ''  
+            }  
+          });  
+      })  
+      .then((response: SPHttpClientResponse): Promise<IListItem> => {  
+        return response.json();  
+      })  
+      .then((item: IListItem): void => {  
+        this.updateStatus(`Item ID1: ${item.Id}`);  
+    
+        const body: string = JSON.stringify({  
+          'EnrollmentCompleted': `${new Date().toJSON()}`  
+        });  
+    
+        this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${item.Id})`,  
+          SPHttpClient.configurations.v1,  
+          {  
+            headers: {  
+              'Accept': 'application/json;odata=nometadata',  
+              'Content-type': 'application/json;odata=nometadata',  
+              'odata-version': '',  
+              'IF-MATCH': '*',  
+              'X-HTTP-Method': 'MERGE'  
+            },  
+            body: body  
+          })  
+          .then((response: SPHttpClientResponse): void => {  
+            $.modal.defaults = { closeExisting: true };
+            $.modal.close();
+            this.updateStatus(`Item with ID: ${latestItemId} successfully updated`);  
+          }, (error: any): void => {  
+            this.updateStatus(`Error updating item: ${error}`);  
+          });  
+      });  
+  }  
+  private updateStatus(status: string, items: IListItem[] = []): void {  
+    //this.domElement.querySelector('.status').innerHTML = status;  
+    //this.updateItemsHtml(items);  
+    console.log(status);
+  }  
+
+
+
+
+
+
+
+
+
+
+  
+  public FormSave_old(): void {
+    let etag: string = undefined;
+    let listItemEntityTypeName: string = "SP.Data.EnrollmentsListItem";
+
+    //const opt: ISPHttpClientOptions = { headers: { 'Content-Type': 'some value' }, body: { my: "bodyJson" } };
+    //this.context.spHttpClient.post('', SPHttpClientConfiguration.);
+
+    this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${this.properties.itemId})?$select=Id`, SPHttpClient.configurations.v1, {
+      headers: {
+        'Accept': 'application/json;odata=nometadata',
+        'odata-version': ''
+      }
+    })
+      .then((response: SPHttpClientResponse): Promise<IListItem> => {
+        etag = response.headers.get('ETag');
+        return response.json();
+      })
+      .then((item: IListItem): Promise<SPHttpClientResponse> => {
+        const postbody = JSON.stringify({__metadata:{'type':'SP.Data.EnrollmentsListItem'},Title:'test update'});
+        //          'ContentTypeId': '0x0100DD259FFD1382FA4B8AD9BB7FE83F2C8A'
+        return this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${item.Id})`, SPHttpClient.configurations.v1, {
+          headers: {
+            "accept": "application/json;odata=verbose",
+            "content-Type": "application/json;odata=verbose",
+            "IF-MATCH": "*",
+            "X-HTTP-Method": "MERGE"
+          },
+          body: "{__metadata:{'type':'SP.Data.EnrollmentsListItem'},Title:'test update'}"
+        });
+      })    //,        'IF-MATCH': etag,        'X-HTTP-Method': 'MERGE'     data: "{__metadata:{'type':'SP.Data.EnrollmentsListItem'},Title:'thisisatest'}"              CompletedEnrollment: '1',         UserEmail: 'jd@projectpointinc.com'
+      .then((response: SPHttpClientResponse): void => {
+        $.modal.defaults = { closeExisting: true };
+        $.modal.close();
+      })
+      .catch((error: any) => {
+        console.log(error);
+        //return true;  ///  log the error and return true so user can continue
+      });
+
+  }
+
   private showIframe(): void {
     let div: HTMLDivElement = document.createElement("div");
     div.id = "ex1";
@@ -145,7 +263,7 @@ export default class SpfxExtensionApplicationCustomizer extends BaseApplicationC
     });
   }
 
-  
+
   public iframeOnload(url: string): void {
     if (url.indexOf("Source") == -1) {
       this.getLatestItemId().then((result) => {
@@ -153,6 +271,7 @@ export default class SpfxExtensionApplicationCustomizer extends BaseApplicationC
         if (this.properties.itemId > 0) {
           window.document.location.href = redirectUrl;
         } else {
+          $(this).modal();
           $.modal.defaults = { closeExisting: true };
           $.modal.close();
         }
@@ -164,105 +283,40 @@ export default class SpfxExtensionApplicationCustomizer extends BaseApplicationC
         });
     }
   }
-  
 
-
-Commented out as no longer used 
-
-
-  private listItemEntityTypeName: string = undefined;
-  private _topPlaceholder: PlaceholderContent | undefined;
-
-
-
-  
-    constructor() {
-      super();
-      SPComponentLoader.loadCss('https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css');
-    }
- 
-  
-    
-
-
-  
-    private showForm(): void {
-      let formDiv:HTMLDivElement = document.createElement("div");
-      formDiv.innerHTML=`
+  private showForm(): void {
+    let formDiv: HTMLDivElement = document.createElement("div");
+    formDiv.innerHTML = `
       <div id="ex1" class="modal">
         <input id="CompleteButton" type="button" value="Complete Enrollment" class="complete" />
         <input id="CancelButton" type="button" value="Cancel Enrollment" class="cancel" />
       </div>
       `;
-      formDiv.querySelector('input.cancel').addEventListener('click', () => { this.FormCancel(); });  // error if not found
-      formDiv.querySelector('input.complete').addEventListener('click', () => { this.FormSave(); });
-  
-      if (!this._topPlaceholder) {
-        this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Top, { onDispose: this._onDispose });
-        if (this._topPlaceholder.domElement) {
-          this._topPlaceholder.domElement.appendChild(formDiv);
-        }
+    formDiv.querySelector('input.cancel').addEventListener('click', () => { this.FormCancel(); });  // error if not found
+    formDiv.querySelector('input.complete').addEventListener('click', () => { this.FormSave(); });
+
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Top, { onDispose: this._onDispose });
+      if (this._topPlaceholder.domElement) {
+        this._topPlaceholder.domElement.appendChild(formDiv);
       }
-  
-      $("#ex1").modal({
-        escapeClose: false,
-        clickClose: false,
-        showClose: false,
-        fadeDuration: 100
-      });
     }
-  
-    public FormCancel(): void {
-      window.document.location.href = redirectUrl;
-    }
-  
-    public FormSave(): void {
-      let etag: string = undefined;
-      let listItemEntityTypeName: string = "SP.Data.EnrollmentsListItem";
-  
-      //const opt: ISPHttpClientOptions = { headers: { 'Content-Type': 'some value' }, body: { my: "bodyJson" } };
-      //this.context.spHttpClient.post('', SPHttpClientConfiguration.);
-  
-      this.context.spHttpClient.get(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${this.properties.itemId})?$select=Id`, SPHttpClient.configurations.v1, {
-          headers: {
-            'Accept': 'application/json;odata=nometadata',
-            'odata-version': ''
-          }
-        })
-        .then((response: SPHttpClientResponse): Promise<IListItem> => {
-          etag = response.headers.get('ETag');
-          return response.json();
-        })
-        .then((item: IListItem): Promise<SPHttpClientResponse> => {
-           const postbody = JSON.stringify({
-            '__metadata': {'type': listItemEntityTypeName},
-            'Title': 'test update',
-            'CompletedEnrollment': '1',
-            'UserEmail': 'jd@projectpointinc.com'
-            });
-                //          'ContentTypeId': '0x0100DD259FFD1382FA4B8AD9BB7FE83F2C8A'
-          return this.context.spHttpClient.post(`${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.properties.listName}')/items(${item.Id})`, SPHttpClient.configurations.v1, {
-              headers: {
-                'Accept': 'application/json;odata=nometadata',
-                'Content-type': 'application/json;odata=verbose',
-                'odata-version': ''
-              },
-              body: postbody
-          });
-        })    //,        'IF-MATCH': etag,        'X-HTTP-Method': 'MERGE'
-        .then((response: SPHttpClientResponse): void => {
-          $.modal.defaults = { closeExisting: true };
-          $.modal.close();
-        })
-        .catch((error: any) => {
-          console.log(error);
-          //return true;  ///  log the error and return true so user can continue
-        });
-  
-    }
-  
+
+    $("#ex1").modal({
+      escapeClose: false,
+      clickClose: false,
+      showClose: false,
+      fadeDuration: 100
+    });
+  }
+
+  public FormCancel(): void {
+    window.document.location.href = redirectUrl;
+  }
+
+
 
   private _onDispose(): void {
     console.log('[HelloWorldApplicationCustomizer._onDispose] Disposed custom top and bottom placeholders.');
   }
-  */
+}
